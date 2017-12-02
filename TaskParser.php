c0d682cad455bc8296c988af770167b14d4897ee
John Lund <dqpresident@gmail.com>
2017-12-01 18:59:31 -0700

    Update regex for tasks
    it works now

--- TaskParser.php
+++ TaskParser.php
@@ -1,10 +1,42 @@
 <?php
 
 class TaskParser {
+	public $title;
+	public $dueDateString;
+	public $durationString;
+	public $category;
+	public $context;
 
 	function __construct($inputString)
 	{
-		preg_match("([^+#@~]*){1} (?:\s\+(.*)|\s\~(.*)|\s\#(.*)|\s\@(.*)){0,4}","hey");
+		preg_match("/([^+#@~]*)(?:(\+[^+#@~]*)|(\~[^+~@#]*)|(\#[^~#+@]*)|(\@[^~#+@]*))*/",$inputString, $matches);
+		$this->title = $matches[1];
+		foreach ($matches as $index=>$match)
+		{
+			if ($index === 0 || $index === 1)
+			{
+				continue;
+			}
+
+			switch (substr($match,0,1))
+			{
+				case '+':
+					$this->dueDateString = ltrim($match, '+');
+					break;
+				case '~':
+					$this->durationString = ltrim($match, '~');
+					break;
+				case '#':
+					$this->category = ltrim($match, '#');
+					break;
+				case '@':
+					$this->context = ltrim($match, '@');
+					break;
+				default:
+					continue;
+			}
+		}
+		//echo print_r($matches, true);
 
 	}
 }
--- gtd.php
+++ gtd.php
@@ -1,10 +1,76 @@
 <?php
+require_once('TaskParser.php');
 //Title +dueDate #category +duration @context
-while ($line !== 'done')
+$thoughts = array();
+$line = "";
+while (true)
 {
-    echo "Enter a task:\n";
+	echo "What's on your mind? (Press Enter to submit)\n";
     $handle = fopen ("php://stdin","r");
-    $line = fgets($handle);
+    $line = trim( fgets($handle) );
+	if ($line === "done")
+	{
+		break;
+	}
+	$thoughts[] = $line;
+	$taskParser = new TaskParser($line);
+	echo print_r($taskParser, true);
+}
+die;
+$defined = array();
+$abstract = array();
+
+foreach ($thoughts as $thought)
+{
+	echo "Can you clearly identify this task as a task, event, or note/reference? (yes/no)\n";
+	echo $thought . "\n";
+	$handle = fopen ("php://stdin","r");
+    $line = trim( fgets($handle) );
+	if (strtolower($line) === "yes")
+	{
+		$defined[] = $thought;
+	}
+	else
+	{
+		$abstract[] = $thought;
+	}
+}
+echo "defined: " . print_r($defined, true);
+echo "abstract: " . print_r($abstract, true);
+
+$tasks = array();
+$events = array();
+$notes = array();
+
+foreach ($defined as $item)
+{
+	echo "Is this a task, event, or note/reference material? (t/e/n)\n";
+	echo $item . "\n";
+	$handle = fopen ("php://stdin","r");
+    $line = trim( fgets($handle) );
+	switch ( strtolower($line) )
+	{
+		case "t":
+			$tasks[] = $item;
+			break;
+		case "e":
+			$events[] = $item;
+			break;
+		case "n":
+			$notes[] = $item;
+			break;
+		default:
+			break;
+	}
 }
 
-echo $line;
+foreach ($tasks as $task)
+{
+	echo "What else do you know about this task?\n";
+	echo "Follow this format: Title +dueDate ~duration #category @context\n";
+	echo "Example: algebra hw 6.8 +monday ~1hr #school @textbook\n";
+	echo $task . "\n";
+	$handle = fopen ("php://stdin","r");
+    $line = trim( fgets($handle) );
+	$taskParser = new TaskParser($line);
+}
