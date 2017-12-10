d0705c909875b34677f3d5f59fc719ffa1554134
John <johnrlund@byu.studentbody.edu>
2017-12-10 11:23:20 -0700

    changes some modules and added mongo

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var mongo = require('mongodb');
 var routes = require('./routes/index');
 var app = express();
 
@@ -19,7 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
-app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
