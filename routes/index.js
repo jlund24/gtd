b77b21b4db6f0c4e18c0df407d8756f5f6fd5c67
John Lund <dqpresident@gmail.com>
2017-12-11 09:49:33 -0700

    updated modules and added a button

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,14 @@ var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/mydb";
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  db.createCollection("thoughts", function(err, res) {
-    if (err) throw err;
-    console.log("Collection created!");
-    db.close();
-  });
-});
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   db.createCollection("thoughts", function(err, res) {
+//     if (err) throw err;
+//     console.log("Collection created!");
+//     db.close();
+//   });
+// });
 
 router.get('/', function(req, res, next) {
     res.render('landing');
