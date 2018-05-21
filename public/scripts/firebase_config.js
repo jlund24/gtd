ad564851083ff23e32c728a873c0ee23ea1c85fb
John <johnrlund@byu.studentbody.edu>
2018-05-20 22:38:47 -0600

    tried adding firebase

---
+++ public/scripts/firebase_config.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyBcmw373Fg2wwdaGrrsSomgEHtVP4V_r2c",
+    authDomain: "cardbasedgtd.firebaseapp.com",
+    databaseURL: "https://cardbasedgtd.firebaseio.com",
+    projectId: "cardbasedgtd",
+    storageBucket: "cardbasedgtd.appspot.com",
+    messagingSenderId: "519828513533"
+};
+firebase.initializeApp(config);
